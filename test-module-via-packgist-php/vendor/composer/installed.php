1eba42ef498920d50fa11995b08d37226300ff39
ridipblog <memorytemp5@gmail.com>
2025-05-15 19:30:55 +0530

    test packgae in php project via github repo

---
+++ test-module-via-packgist-php/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yourname/test-module-via-packgist-php',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4e178611c14d4f7fc7d405df666e532418706117',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yourname/test-module-via-packgist-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4e178611c14d4f7fc7d405df666e532418706117',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yourvendor/mypackage' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '85d1f3a42664b9bcd63b02c440ced474e086ed75',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yourvendor/mypackage',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
