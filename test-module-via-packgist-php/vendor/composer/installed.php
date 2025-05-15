0ab3c71659a02558cd03b6c601132c1e4151ef49
ridipblog <memorytemp5@gmail.com>
2025-05-15 19:43:04 +0530

    test module via packgist php project

---
+++ test-module-via-packgist-php/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5df25c7096e5268be4720718cb84b18ffd2da454',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5df25c7096e5268be4720718cb84b18ffd2da454',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yourvendor/mypackage' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '85d1f3a42664b9bcd63b02c440ced474e086ed75',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yourvendor/mypackage',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
